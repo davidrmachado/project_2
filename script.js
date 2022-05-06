32e35b744639843af03d99d63918a4dda14e8dee
J. Gouveia <gouvik.dev@gmail.com>
2022-05-06 17:29:07 -0300

    corrigindo lint

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-/* eslint-disable no-unused-expressions */
 const btnEntrar = document.getElementById('btn-entrar');
 const agreementCheck = document.getElementById('agreement');
 
 btnEntrar.addEventListener('click', () => {
   const email = document.getElementById('inputEmail').value;
   const senha = document.getElementById('inputPassword').value;
-  email === 'tryber@teste.com' && senha === '123456'
-    ? alert('Olá, Tryber!') : alert('Email ou senha inválidos.');
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 });
 
 agreementCheck.addEventListener('click', () => {
